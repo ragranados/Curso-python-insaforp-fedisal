9b26745c8f92a184e146aeba6eed65c1af8e733d
Raúl Granados <raul.granados12@gmail.com>
2021-11-17 17:27:43 -0600

    20211117

---
+++ Primero Modulo/20211117/main.py
@@ -0,0 +1,15 @@
+print("Hola mundo")
+
+saludo = "que pedos :V"
+print(type(saludo))
+
+numero = 4
+print(type(numero))
+
+precio = 26262.2
+print(type(precio))
+
+es_blanco = True
+print(type(es_blanco))
+
+#En python se recomienda utilizar snake case (Revisa documento PEP 8).
