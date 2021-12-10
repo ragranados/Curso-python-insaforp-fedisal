ce9474571122b486eebb889a04b895e2f65fe647
Raúl Granados <raul.granados12@gmail.com>
2021-12-10 17:50:04 -0600

    20211210

--- Primero Modulo/20211210 - Sesion 8/evaluacion/imprimir.py
+++ Primero Modulo/20211210 - Sesion 8/evaluacion/imprimir.py
@@ -3,6 +3,5 @@ from factura import Factura
 class ImprimirPDF:
 
     def imprimir_factura(self, factura: Factura) -> None:
-        total = 0
         print('Informacion factura: \n')
         factura.__str__()
